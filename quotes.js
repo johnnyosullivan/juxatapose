7f7d13f3d098ad882368700836afdf4a57b39616
johnnyosullivan <johnnyosullivan>
2019-08-13 23:12:26 +0000

    lets-go-again

--- quotes.js
+++ quotes.js
@@ -1,98 +1,98 @@
 const quotes = [
 					{
 					id: 1, 
-				 	quote: "The Way Get Started Is To Quit Talking And Begin Doing.", 
+				 	quote: "The way to get started is to quit talking and begin doing.", 
 				 	author: "Walt Disney",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 2, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
+				 	quote: "The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.", 
 				 	author: "",
-					type: ["humorous", "inspirational"]
+					type: ["Winston Churchill", "inspirational"]
 					},
 					{
 					id: 3, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "Act as if what you do makes a difference. It does.", 
+				 	author: "William James",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 4, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.", 
+				 	author: "Winston Churchill",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 5,
-					quote: "a funny quote to end all qoutes",
-					author: "",
+					quote: "Never bend your head. Always hold it high. Look the world straight in the eye.",
+					author: "Helen Keller",
 					type: ["", ""],	
-					}
+					},
 					{
 					id: 6, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "What you get by achieving your goals is not as important as what you become by achieving your goals.", 
+				 	author: "Zig Ziglar",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 7, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "Believe you can and you're halfway there.", 
+				 	author: "Theodore Roosevelt",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 8, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "When you have a dream, you've got to grab it and never let go.", 
+				 	author: "Carol Burnett",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 9, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.", 
+				 	author: "Jimmy Dean",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 10, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.", 
+				 	author: "William James",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 11, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "No matter what you're going through, there's a light at the end of the tunnel.", 
+				 	author: "Demi Lovato",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 12, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.", 
+				 	author: "Albert Einstein",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 13, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "Just don't give up trying to do what you really want to do. Where there is love and inspiration, I don't think you can go wrong.", 
+				 	author: "Ella Fitzgerald",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 14, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "Limit your 'always' and your 'nevers.'', 
+				 	author: "Amy Poehler",
 					type: ["humorous", "inspirational"]
 					},
 					{
 					id: 15, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+				 	quote: "Nothing is impossible. The word itself says "I'm possible!"", 
+				 	author: "Audrey Hepburn",
 					type: ["humorous", "inspirational"]
 					},
 					{
-					id: 1, 
-				 	quote: "the only women who came came late for the ball and it worked out was cinderella", 
-				 	author: "",
+					id: 16, 
+				 	quote: "You are never too old to set another goal or to dream a new dream.", 
+				 	author: "C.S. Lewis",
 					type: ["humorous", "inspirational"]
 					},
 					{
